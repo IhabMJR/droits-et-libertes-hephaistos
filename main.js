c60faada5036ece7c605ff6fe6f133981c35449b
Justinersu <112107976+Justinersu@users.noreply.github.com>
2024-09-24 16:59:33 -0400

    Finalisation de la section héro
    - Ajout des mixins media queries
    - Ajout du swiper
    - Ajout du titre (et sous-titre)
    - Ajout du bouton revue
    - Responsive (phone, tablet, desktop)

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// section hero Swiper
+//--- Hero Swiper ---
+const swiper = new Swiper(".swiper", {
+  speed: 400,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+});
