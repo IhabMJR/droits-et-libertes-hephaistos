00e7618a73da4263985dc20cc45eabdf5f3c6d6d
RandomizedRaptor <i.mseriousstuff@gmail.com>
2024-09-29 20:00:36 -0400

    navbar animation complete

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+//--- Navbar scroll ---
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 56) {
+    navbar.classList.add('navbar-scrolled');
+  } else if (window.scrollY < 56) {
+    navbar.classList.remove('navbar-scrolled');
+  }
+});
+
 //--- Hero Swiper ---
 const swiper = new Swiper(".swiper", {
   speed: 400,
@@ -9,3 +20,5 @@ const swiper = new Swiper(".swiper", {
     type: "bullets",
   },
 });
+
+//--- Navbar scroll ---
