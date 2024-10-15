4df7c02415c5a9883ec40b27c50d33736d20a6f0
RandomizedRaptor <i.mseriousstuff@gmail.com>
2024-10-15 00:56:11 -0400

    wordpress install + loop sur hero

---
+++ wp-content/themes/ldl/main.js
@@ -0,0 +1,31 @@
+//--- Navbar scroll ---
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 56) {
+    navbar.classList.add('navbar-scrolled');
+  } else if (window.scrollY < 56) {
+    navbar.classList.remove('navbar-scrolled');
+  }
+});
+
+//--- Hero Swiper ---
+const swiper = new Swiper(".swiper-hero", {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+});
+
+//--- Swiper temoignages ---/
+const swiperTemoignages = new Swiper(".swiper-temoignages", {
+  speed: 400,
+  autoplay: {
+    delay: 3000,
+  },
+});
